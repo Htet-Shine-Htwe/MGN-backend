579df515fd0fec2e28c482688733bc704ae791e2
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-08-23 15:21:44 +0630

    test:add new 2 stress test

--- tests/Feature/Stress/General/BasicTest.php
+++ tests/Feature/Stress/General/BasicTest.php
@@ -1,12 +1,47 @@
 <?php
 
 use function Pest\Stressless\stress;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\MogousCategorySeeder;
+use Database\Seeders\MogouSeeder;
+use Database\Seeders\SubMogouSeeder;
+use Database\Seeders\SubscriptionSeeder;
 
-it('has a fast response time', function () {
+beforeEach(function() {
+    config(['control.test.mogous_count' => 20]);
+
+    // Seed the database
+    $this->seed([
+        SubscriptionSeeder::class,
+        CategorySeeder::class,
+        MogouSeeder::class,
+        MogousCategorySeeder::class,
+        SubMogouSeeder::class,
+    ]);
+
+});
+
+uses()->group('stress-test');
+
+it('home page carousel response under 100ms ', function () {
     // $result = stress('127.0.0.1:8000');s
-    $host = env('APP_URL');
+    $host =route('api.users.carousel');
 
-    $result = stress($host)->concurrency(5)->for(5)->seconds()->dump();
+    $result = stress($host)->concurrency(100)->for(3)->seconds();
+
+    expect($result->requests()->failed()->rate())->toBeLessThan(2);
 
     expect($result->requests()->duration()->med())->toBeLessThan(100); // < 100.00ms
 })->group('stress-test');
+
+it("most-viewed mogous response under 100ms",function(){
+    $host =route('api.users.most-viewed');
+
+    $result = stress($host)->concurrency(100)->for(3)->seconds();
+
+    expect($result->requests()->failed()->rate())->toBeLessThan(2);
+
+    expect($result->requests()->duration()->med())->toBeLessThan(100); // < 100.00ms
+});
+
+
