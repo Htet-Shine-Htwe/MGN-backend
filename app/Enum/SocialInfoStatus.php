4cf6e6ff3c164a578cf4aef56b639cba757d2856
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-09-11 10:47:21 +0630

    dev : update soical info status

---
+++ app/Enum/SocialInfoStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enum;
+
+enum SocialInfoStatus : int
+{
+    case Active = 1;
+    case Inactive = 0;
+
+    public function isActive(): bool
+    {
+        return $this->value === self::Active;
+    }
+
+    public function isInactive(): bool
+    {
+        return $this->value === self::Inactive;
+    }
+
+    public function getLabel(): string
+    {
+        return match ($this->value) {
+            self::Active => 'Active',
+            self::Inactive => 'Inactive',
+        };
+    }
+
+}
