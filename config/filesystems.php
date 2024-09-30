8541ed9d3fefe41b998c5fb84f089e9880c0477f
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-09-30 23:48:28 +0630

    dev : add backup config

--- config/filesystems.php
+++ config/filesystems.php
@@ -8,23 +8,23 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
+    | by the framework. The 'local' disk, as well as a variety of cloud
     | based disks are available to your application. Just store away!
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env( 'FILESYSTEM_DISK', 'local' ),
 
     /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
-    | Here you may configure as many filesystem "disks" as you wish, and you
+    | Here you may configure as many filesystem 'disks' as you wish, and you
     | may even configure multiple disks of the same driver. Defaults have
     | been set up for each driver as an example of the required values.
     |
-    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    | Supported Drivers: 'local', 'ftp', 'sftp', 's3'
     |
     */
 
@@ -32,31 +32,42 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path( 'app/public' ),
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path( 'app/public' ),
+            'url' => env( 'APP_URL' ).'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env( 'AWS_ACCESS_KEY_ID' ),
+            'secret' => env( 'AWS_SECRET_ACCESS_KEY' ),
+            'region' => env( 'AWS_DEFAULT_REGION' ),
+            'bucket' => env( 'AWS_BUCKET' ),
+            'url' => env( 'AWS_URL' ),
+            'endpoint' => env( 'AWS_ENDPOINT' ),
+            'use_path_style_endpoint' => env( 'AWS_USE_PATH_STYLE_ENDPOINT', false ),
             'visibility' => 'public',
             'throw' => false,
         ],
 
+        'backups' => [
+            'driver' => 's3',
+            'key' => env( 'AWS_ACCESS_KEY_ID' ),
+            'secret' => env( 'AWS_SECRET_ACCESS_KEY' ),
+            'region' => env( 'AWS_DEFAULT_REGION' ),
+            'bucket' => env( 'AWS_BUCKET' ),
+            'url' => env( 'AWS_URL' ),
+            'endpoint' => env( 'AWS_ENDPOINT' ),
+            'use_path_style_endpoint' => env( 'AWS_USE_PATH_STYLE_ENDPOINT', false ),
+            'root' => 'backup', // specify the root directory for backups
+        ],
     ],
 
     /*
@@ -71,7 +82,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('/app/public'),
+        public_path( 'storage' ) => storage_path( '/app/public' ),
     ],
 
 ];
