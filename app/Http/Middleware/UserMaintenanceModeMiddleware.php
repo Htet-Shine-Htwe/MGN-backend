488e27a74b33e10d37713dcaab95751b66392393
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-10-19 21:23:47 +0630

    dev : client side middleware

---
+++ app/Http/Middleware/UserMaintenanceModeMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\ApplicationConfig;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserMaintenanceModeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $is_maintenance_mode = ApplicationConfig::first()->user_side_is_maintenance_mode;
+        if ($is_maintenance_mode) {
+            return response()->json([
+                'message' => 'The application is in maintenance mode. Please try again later.'
+            ], 503);
+        }
+        return $next($request);
+    }
+}
