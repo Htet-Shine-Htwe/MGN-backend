d7f4edfd05a08f915c08f4e775266a9e386aefd3
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-11-07 18:36:11 +0630

    dev : worker test

---
+++ app/Jobs/RecordLoginAddress.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Services\ClientIp\ClientIpAddressService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class RecordLoginAddress implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $tries = 3;
+
+    public int $timeout = 60;
+
+    public User $user;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $clientIp = app(ClientIpAddressService::class);
+
+        Log::info('RecordLoginAddress', ['user' => $this->user]);
+
+        $clientIp->saveRecord($this->user);
+
+    }
+
+}
